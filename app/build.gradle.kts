1bf77b478d1dcae92ba0b9e4f27ed1a02818cb59
five5-7 <bapydfxf@outlook.com>
2026-08-15 22:29:23 +0800

    FocusFlow 2.5：弹性任务初步规划

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.sakata.focusflow"
         minSdk = 26
         targetSdk = 35
-        versionCode = 240
-        versionName = "2.4"
+        versionCode = 250
+        versionName = "2.5"
     }
 
     compileOptions {
