e85fd4642efa33786b99c8f24a7339bffc1a9d53
five5-7 <bapydfxf@outlook.com>
2026-08-15 18:06:04 +0800

    chore: bump hotfix version to 1.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.sakata.focusflow"
         minSdk = 26
         targetSdk = 35
-        versionCode = 105
-        versionName = "1.5"
+        versionCode = 106
+        versionName = "1.6"
     }
 
     compileOptions {
