b627dfba237d2d4b3d7a8a546d5bd24851267c93
five5-7 <bapydfxf@outlook.com>
2026-08-25 16:58:39 +0800

    Fix Compose layout imports

--- app/src/main/java/com/sakata/focusflow/ScheduleScreen.kt
+++ app/src/main/java/com/sakata/focusflow/ScheduleScreen.kt
@@ -1,15 +1,7 @@
 package com.sakata.focusflow
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.weight
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
