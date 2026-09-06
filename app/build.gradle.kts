91ce7e958f70f3a69d0a86a83f062ec3f4ad857d
five5-7 <bapydfxf@outlook.com>
2026-09-06 21:50:03 +0800

    chore: bump 7.7 release candidate version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.sakata.focusflow"
         minSdk = 26
         targetSdk = 35
-        versionCode = 493
-        versionName = "7.6.0-rc.3"
+        versionCode = 494
+        versionName = "7.7.0-rc.1"
         buildConfigField("String", "CI_RUN_NUMBER", "\"$focusFlowCiRun\"")
     }
 
