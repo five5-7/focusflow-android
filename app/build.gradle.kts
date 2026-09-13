5f65d11619b56e308622f61fc422be626fb78dc8
five5-7 <bapydfxf@outlook.com>
2026-09-13 20:10:29 +0800

    chore: prepare 8.2.1 rc7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.sakata.focusflow"
         minSdk = 26
         targetSdk = 35
-        versionCode = 531
-        versionName = "8.2.1-rc.6"
+        versionCode = 532
+        versionName = "8.2.1-rc.7"
         buildConfigField("String", "CI_RUN_NUMBER", "\"$focusFlowCiRun\"")
     }
 
