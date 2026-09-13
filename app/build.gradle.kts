3b2d090933e8db1013cf641bb4e561e665904e26
five5-7 <bapydfxf@outlook.com>
2026-09-13 21:36:54 +0800

    release: promote 8.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.sakata.focusflow"
         minSdk = 26
         targetSdk = 35
-        versionCode = 533
-        versionName = "8.2.1-rc.8"
+        versionCode = 534
+        versionName = "8.2.1"
         buildConfigField("String", "CI_RUN_NUMBER", "\"$focusFlowCiRun\"")
     }
 
