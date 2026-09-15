0391760e1f234cc27f46fac474cb3d8484ae473d
five5-7 <bapydfxf@outlook.com>
2026-09-15 10:47:36 +0800

    chore: freeze 8.3.0-rc.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.sakata.focusflow"
         minSdk = 26
         targetSdk = 35
-        versionCode = 542
-        versionName = "8.3.0-rc.8"
+        versionCode = 543
+        versionName = "8.3.0-rc.9"
         buildConfigField("String", "CI_RUN_NUMBER", "\"$focusFlowCiRun\"")
     }
 
