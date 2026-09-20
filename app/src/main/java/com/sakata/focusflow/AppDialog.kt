bf830103c3a74095c5f7d018ba0638902559f224
five5-7 <bapydfxf@outlook.com>
2026-09-20 13:23:41 +0800

    fix: import adaptive dialog layout APIs

--- app/src/main/java/com/sakata/focusflow/AppDialog.kt
+++ app/src/main/java/com/sakata/focusflow/AppDialog.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
