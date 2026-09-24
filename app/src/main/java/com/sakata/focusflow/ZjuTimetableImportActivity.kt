809ce1ee18c6c708013eafc22c6417728ebf5b66
five5-7 <bapydfxf@outlook.com>
2026-09-24 16:55:43 +0800

    Opt in to semester selector flow layout

--- app/src/main/java/com/sakata/focusflow/ZjuTimetableImportActivity.kt
+++ app/src/main/java/com/sakata/focusflow/ZjuTimetableImportActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -380,6 +381,7 @@ private fun ImportHeader() {
     }
 }
 
+@OptIn(ExperimentalLayoutApi::class)
 @Composable
 private fun CredentialCard(
     username: String,
